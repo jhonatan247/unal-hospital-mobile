0ed0c019da68976605a8b650bf91032d8054ca3e
jhonatan torres <jtorresf@artyficial.net>
2020-03-27 22:11:20 -0500

    base project was created

---
+++ android/app/src/main/kotlin/co/edu/unal/me_cuido/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.unal.me_cuido
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
